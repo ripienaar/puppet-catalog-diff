766669b56cbc4fffa29539b472e86f7c73163913
Zack Smith <zack@puppetlabs.com>
2013-11-05 18:21:24 +0000

    Attempt to turn node_percent to float
    Prior to this commit the node_percent was rounded, this commit
    changes it to a flot

--- lib/puppet/catalog-diff/differ.rb
+++ lib/puppet/catalog-diff/differ.rb
@@ -92,7 +92,7 @@ module Puppet::CatalogDiff
       output[:resource_changes]   = "#{(!additions.zero?  && "+#{additions}" || 0)} / #{(!subtractions.zero?  && "-#{subtractions}" || 0)}"
 
       divide_by = (changes_percentage.zero? ? 0 : 1) + (additions_percentage.zero? ? 0 : 1) + (subtractions_percentage.zero? ? 0 : 1)
-      output[:node_percentage]      = (additions_percentage == 100 && 100 || ('%.2f' % ((changes_percentage + additions_percentage + subtractions_percentage) / divide_by ))).to_f
+      output[:node_percentage]      = (divide_by == 0 && 0 || additions_percentage == 100 && 100 || (changes_percentage + additions_percentage + subtractions_percentage) / divide_by ).to_f
       output
     end
   end
