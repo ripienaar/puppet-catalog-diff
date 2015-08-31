4ce6356bc1c218980b26895afbbe30133b86c80f
Zack Smith <zack@puppetlabs.com>
2015-08-31 14:50:12 -0700

    Add debug back

--- lib/puppet/face/catalog/pull.rb
+++ lib/puppet/face/catalog/pull.rb
@@ -67,9 +67,7 @@ Puppet::Face.define(:catalog, '0.0.1') do
 
       thread_count.times.map {
         Thread.new(nodes,compiled_nodes,options) do |nodes,compiled_nodes,options|
-         if JSON.load(nodes).has_key?('issue_kind')
-           raise "Not authorized to retrieve facts, auth.conf edits missing?" if facts_object['issue_kind'] == 'FAILED_AUTHORIZATION'
-         end
+         Puppet.debug(nodes)
          while node_name = mutex.synchronize { nodes.pop }
             begin
               if nodes.size.odd?
