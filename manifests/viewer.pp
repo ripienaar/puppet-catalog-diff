c2e91dcbc68a351ded9f0f50af8b74aaf5f1c6ee
Zack Smith <zack@puppetlabs.com>
2015-09-04 10:21:21 -0700

    Add more advanced apache vhost config
    Prior to this commit we listened on the default
    apache vhost this make it a little more explicit

--- manifests/viewer.pp
+++ manifests/viewer.pp
@@ -1,11 +1,44 @@
-class catalog_diff::viewer {
-  require apache
+class catalog_diff::viewer (
+  $remote   = 'https://github.com/camptocamp/puppet-catalog-diff-viewer.git',
+  $password = 'puppet',
+  $revision = 'master',
+) {
   require git
 
-  vcsrepo { '/var/www/html':
+  class {'apache':
+    default_vhost_ensure     => false,
+    default_ssl_vhost_ensure => false,
+  }
+
+  apache::listen { '1495': }
+
+  apache::vhost { 'catalog-diff-website':
+    ip          => $::ipaddress,
+    docroot     => '/var/www/diff',
+    ip_based    => true,
+    directories => [
+      { path           => '/var/www/diff',
+        auth_type      => 'basic',
+        auth_name      => 'Catalog Diff',
+        auth_user_file => '/var/www/.htpasswd',
+        auth_require   => 'valid-user',
+      },
+    ],
+    require     => Htpasswd['puppet'],
+  }
+
+  htpasswd { 'puppet':
+    username    => 'puppet',
+    cryptpasswd => ht_sha1($password),
+    target      => '/var/www/.htpasswd',
+    require     => Class['apache'],
+  }
+
+  vcsrepo { '/var/www/diff':
     ensure   => latest,
     provider => 'git',
-    source   => 'https://github.com/camptocamp/puppet-catalog-diff-viewer.git',
-    revision => 'master',
+    source   => $remote,
+    revision => $revision,
+    require  => Class['apache'],
   }
 }
